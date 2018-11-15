75c294caaddbd7b2b2c59a152adcc99166457c6d
Monitor <jerson.condor@vallegrande.edu.pe>
2018-11-15 13:28:41 -0500

    archivos de js

---
+++ Lab02/js/variable.js
@@ -0,0 +1,96 @@
+//Definiendo variables
+var nombre = "Fernando";
+//imprimir contenido de la variable nombre
+console.log(nombre);
+//definiendo datos personales con variables
+var nomPersona = "Gloria";
+var apePersona = "Gerra Paz";
+var edadPersona = 20;
+var procPersona = "San Vicente";
+//imprimir datos personales
+console.log("MI nombre es " + nomPersona + "y mis apellidos son" + apePersona);
+console.log("MI edad es " +edadPersona + " y soy del distrito" + procPersona)
+//definiendo variables numéricas y calculos respectivos
+var num1 =100
+var num2 =350
+var suma = num1 + num2;
+var resta = num2 - num1;
+var producto = num1 * num2;
+var divide = num2 / num1;
+var mayor = num2 > num1;
+var comparar = num1 == num2;
+
+//imprimir resulatdos
+console.log(" La suma del " + num1 + " mas " +num2 + " es " +suma);
+console.log(" La resta del " + num2 + " menos " + num1 + " es " + resta);
+console.log(" El Producto " + num1 + " por " + num2 + " es " + producto);
+console.log(" la Divicion de " + num2 + " entre " + num1 + " es "+ divide);
+console.log(" El mayor de " + num2 + " y " + num1 + " es " + mayor);
+console.log(" La comparación de " + num1 + " y " + num2 + " es " + comparar);
+
+
+
+
+
+
+
+
+
+//resolver
+var producto = "zapatillas Nike";
+var cantidad = 10;
+var precio = 380;
+var total = cantidad * precio;
+
+//imprimir los resultados
+console.log(" el precio por las sapatilas " + producto + " es " + total );
+
+
+
+
+
+
+
+
+
+
+
+
+
+//definir array con los dias de la semana
+var diasSemana  = [" Lunes ", " Martes ", " Miércoles ", " Jueves ", " Viernes ", " Sabado ", " Domingo ",];
+//imprimiendo el contenido de la variable de tipo array
+console.log(diasSemana);
+console.log(diasSemana[4]);
+
+//crear un array con 5 nombres de personas e imprimirlos en pantalla individual y en grupal
+var nomPersona = [" juan ", " Jerson ", " Martin ", " Alberto ", " Pedro ", " Josue ",];
+console.log(nomPersona);
+console.log(nomPersona [0]);
+console.log(nomPersona[1]);
+console.log(nomPersona[2]);
+console.log(nomPersona[3]);
+console.log(nomPersona[4]);
+console.log(nomPersona[5]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
