ab665b753cb79a7a9694d617bea0191b0884fa94
Monitor <jerson.condor@vallegrande.edu.pe>
2018-11-15 10:05:29 -0500

    archivos js

---
+++ Lab01/js/holamundo.js
@@ -0,0 +1,16 @@
+//Mi primer mensaje por consola
+console.log("Hola terricola.... te escribo desde el mas alla... ");
+//imprimir otro mensaje de prueba
+console.log("este es otero mensaje... no te olvides como hacerlo...");
+//Imprimer numer
+console.log(3.580);
+//imprimir 5 nombres de distrito
+console.log("Imperial,San Vicente,Mala,Asia,Bujama");
+//Sumar los numeros 100 y 200
+console.log(100+500);
+//comparar los siguientes numeros 800y 500
+console.log(800<500);
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Mis primeros pasos con JavaScript
 
 Empesaremos a conocer el leguaje de programación JavaScript
+**descripción de laboratorio**
+**laboratorio 01:**En este 
+laboratorio hemos vinculado un archivo
+JS y ademas hemos ingresado texto y
+numeros en la consola del navegador 
